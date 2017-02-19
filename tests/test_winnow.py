f6cacb36402f5308a93c8169ca1cd5a396f0af46
Brian Schiller <bgschiller@gmail.com>
2017-02-19 14:00:46 -0600

    simplify first filter

--- tests/test_winnow.py
+++ tests/test_winnow.py
@@ -1,12 +1,13 @@
-from core import Winnow
 from nose.tools import assert_equals
+
 from .utils import squish_ws
+from core import Winnow
 
 sources = [
     dict(display_name='Number Scoops', column='num_scoops',
          value_types=['numeric', 'nullable']),
     dict(display_name='Flavor', column='flavor',
-         value_types=['string'])
+         value_types=['collection']),
 ]
 
 ice_cream_winnow = Winnow('ice_cream', sources)
@@ -14,25 +15,24 @@ ice_cream_winnow = Winnow('ice_cream', sources)
 
 def test_basic():
     ice_cream_filt = dict(
-        logical_op='|',
+        logical_op='&',
         filter_clauses=[
-            dict(data_source='Flavor', operator='is', value='Chocolate'),
-            dict(
-                logical_op='&',
-                filter_clauses=[
-                    dict(data_source='Number Scoops', operator='>=', value='2'),
-                    dict(data_source='Flavor', operator='is', value='Strawberry')
-                ])
-        ])
+            dict(data_source='Number Scoops', operator='>=', value='2'),
+            dict(data_source='Flavor', operator='any of', value=[
+                'Strawberry',
+                'Chocolate',
+            ])
+    ])
     query, params = ice_cream_winnow.query(ice_cream_filt)
     expected = '''
     SELECT * FROM ice_cream
-    WHERE
-       ((flavor = %s ) OR
-        ((num_scoops >= %s) AND (flavor = %s )))'''
+    WHERE ((num_scoops >= %s) AND (flavor IN (%s,%s) ))'''
 
     assert_equals(squish_ws(query), squish_ws(expected))
-    assert_equals(params, ('Chocolate', 2, 'Strawberry'))
+    assert_equals(params, (2, 'Strawberry', 'Chocolate'))
+
+def test_filters_can_by_nested():
+    pass
 
 
 def test_special_case():
@@ -40,7 +40,4 @@ def test_special_case():
 
 
 def test_special_case_override():
-    pass
-
-def test_subclass_for_custom_fields():
     pass
