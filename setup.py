2ec891e7ebf58ca781a2242b75681690359d502e
Brian Schiller <bgschiller@gmail.com>
2017-05-04 13:44:33 -0500

    preparing to push to pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name='winnow-filters',
+    packages=['winnow-filters'],
+    version='0.0.1',
+    description='Winnow is a framework for server-side filtering of data.',
+    author='Brian Schiller',
+    author_email='bgschiller@gmail.com',
+    url='https://github.com/bgschiller/winnow',
+    download_url='https://github.com/bgschiller/winnow/archive/0.0.1.tar.gz',
+    keywords=['sql', 'jinja2', 'filters'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+        'jinjasql',
+        'python-dateutil',
+    ],
+)
